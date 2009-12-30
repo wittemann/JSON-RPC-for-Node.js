4665680e27815557c1e8aaabc3ad37f9ca41c4cf
wittemann <martin.wittemann@1und1.de>
2009-12-30 11:26:34 +0100

    Minized the model test file.

--- modultest.js
+++ modultest.js
@@ -1,6 +1,3 @@
-var sys = require('sys'), 
-   rpc = require('./lib/jsonrpc');
-rpc.service = require("./JSON-RPC/service");
-
+var rpc = require('./jsonrpc');
+rpc.service = require("./service");
 rpc.createServer().listen(8000);
-sys.puts('Server running at http://127.0.0.1:8000/');
